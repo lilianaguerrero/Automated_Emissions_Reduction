10b4d8ba6fb18c22e1ae9f85a27009353de8ee71
LilianaGuerrero <liliana.guerrerosf@gmail.com>
2020-08-11 12:41:14 -0700

    removed support files, implemented CO2 algo

--- refrigerator_sim.py
+++ refrigerator_sim.py
@@ -26,29 +26,28 @@ def main():
     env.process(month_simulation(env))
     env.run(until=744) #744 hours in month of March
     runtime = statuses.count('on')*5 #in minutes
-    total_CO2 = 0
-    for status in statuses:
-        if status == 'on':
-            total_CO2 += 200
-
-
-
-    print('The refrigerator runtime for March is ' + str(runtime) + ' minutes or ' + str(runtime/60) + ' hours')
-    # print(rfg_temps,monthly_MOERS,statuses)
-    # plt.plot(monthly_timestamps, rfg_temps)
-    # plt.plot(monthly_timestamps, statuses)
-    # plt.show()
-    print("Simulation complete")
+    print('The total amount of CO2 for March 2019 is ' + str(incremental_CO2) + ' lbs of CO2')
+    print('The total refrigerator runtime for March 2019 is ' + str(runtime) + ' minutes or ' + str(runtime/60) + ' hours')
+    plt.plot(monthly_timestamps, rfg_temps)
+    plt.plot(monthly_timestamps, monthly_MOERS)
+    plt.plot(monthly_timestamps, statuses)
+    plt.plot(monthly_timestamps, lbs_CO2)
+    plt.show()
 
 temp = 33 #starting rfg temp
 statuses = []
 rfg_temps = []
+on_MOERS = []
+lbs_CO2 = []
+incremental_CO2 = 0
 def month_simulation(env):
     #runs hourly simulations over the period of a month
     n = 0 #five minute interval as given in the CSV data
     global temp
     global statuses
     global rfg_temps
+    global lbs_CO2
+    global incremental_CO2
     while True:
         hour = monthly_timestamps[n:n+12] #twelve five minute intervals per hour
         hourly_MOERS = monthly_MOERS[n:n+12]
@@ -58,33 +57,30 @@ def month_simulation(env):
                 statuses.append('off')
                 temp += (5/12)
                 rfg_temps.append(temp)
+                lbs_CO2.append(incremental_CO2)
         elif temp >= 37.99:
             for MOER in hourly_MOERS:
                 if MOER in lowest_MOERS:
                     statuses.append('on')
                     temp -= (10/12)
+                    incremental_CO2 += (MOER * .0002 * 1/12) #in lbs*C02 since MW*(10^-6)= W & 5min = hr/12
                     rfg_temps.append(temp)
+                    on_MOERS.append(MOER)
                     lowest_MOERS.remove(MOER)
                 else:
                     statuses.append('off')
                     temp += (5/12)
                     rfg_temps.append(temp)
+                lbs_CO2.append(incremental_CO2)
         n += 12 #twelve five minute intervals per hour
         hourly_duration = 1 #run this code for every hour during the month
         yield env.timeout(hourly_duration)
 
-# def plot():
-#     plt.plot(monthly_timestamps, monthly_MOERS)
-#     plt.plot(monthly_timestamps, statuses)
-#     plt.xlabel('March 2019 Timestamps')
-#     plt.ylabel('Real Time MOER')
-#     plt.title('Automated Emissions Reduction System')
-#     plt.show()
 
 if __name__ == '__main__':
     process_csv()
     main()
-    # plot()
+    
 
 
 
